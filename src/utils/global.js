10420402f3f0f61d7b98cad34f9ffa05403e51ef
张承 <star.cheung@live.cn>
2018-09-23 16:47:30 +0800

    1

--- src/utils/global.js
+++ src/utils/global.js
@@ -5,7 +5,7 @@
 //本地环境
 //global.baseUrl = "http://localhost:3030";
 //正式环境
-global.baseUrl = "http://working.admin.rzzc.ltd";
+global.baseUrl = "http://working.api.rzzc.ltd";
 //api全局路由
 global.url = {
   login: '/admin/login', //用户登录
